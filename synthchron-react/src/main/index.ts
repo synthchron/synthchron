42eea736f97d584aeea37f4610c7e26bafb8243b
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-15 10:35:06 +0100

    remove redundant files

--- synthchron-react/src/main/index.ts
+++ synthchron-react/src/main/index.ts
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import icon from '../../resources/icon.png'
 
 function createWindow(): void {
   // Create the browser window.
