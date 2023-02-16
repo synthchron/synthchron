df17fc132014c5d8fec2d38be9556a7f4c85fc9f
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-16 14:22:52 +0100

    add collaborative

---
+++ yjs-rf/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Flow from './collaborative-example/App'
+
+function App() {
+  return (
+    <div style={{
+      height: '100vh',
+      width: '100vw',
+    }}>
+      <Flow/>
+    </div>
+  )
+}
+
+export default App
