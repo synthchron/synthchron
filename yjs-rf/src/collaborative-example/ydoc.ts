75b9ffa76ea3516171e3ea08b155702694376f69
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-16 15:36:09 +0100

    move to websockets

--- yjs-rf/src/collaborative-example/ydoc.ts
+++ yjs-rf/src/collaborative-example/ydoc.ts
@@ -2,9 +2,9 @@ import { Doc } from 'yjs';
 // For this example we use the WebrtcProvider to synchronize the document
 // between multiple clients. Other providers are available.
 // You can find a list here: https://docs.yjs.dev/ecosystem/connection-provider
-import { WebrtcProvider } from 'y-webrtc';
+import { WebsocketProvider } from 'y-websocket';
 
 const ydoc = new Doc();
-new WebrtcProvider('REACTFLOW-COLLAB-EXAMPLE', ydoc, {signaling: ['ws://4.tcp.eu.ngrok.io:16118']});
+const wsProvider = new WebsocketProvider('ws://eager-cows-feel-130-225-188-129.loca.lt/', 'my-roomname', ydoc)
 
 export default ydoc;
