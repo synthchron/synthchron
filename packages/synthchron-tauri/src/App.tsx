c81ae990c65d9ef6afea37fd69621d5f2e9b23a4
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-17 11:19:04 +0100

    switch from lerna to turbo

--- packages/synthchron-tauri/src/App.tsx
+++ packages/synthchron-tauri/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { main } from '@synthchron/simulator';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -28,7 +29,7 @@ function App() {
         </a>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <p>Click on the Tauri, Vite, and React logos to learn more. {main()}</p>
 
       <div className="row">
         <form
