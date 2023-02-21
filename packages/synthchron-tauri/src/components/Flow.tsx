2043e0bd71e88e31a997fdc3a0eab22cab18005f
daresoul <nicolas.dyhrman@gmail.com>
2023-02-21 15:02:11 +0100

    added restrictinos on connections in react-flow

--- packages/synthchron-tauri/src/components/Flow.tsx
+++ packages/synthchron-tauri/src/components/Flow.tsx
@@ -1,21 +1,25 @@
 import { useCallback } from 'react';
 import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-  MarkerType,
-  ConnectionMode
+	MiniMap,
+	Controls,
+	Background,
+	useNodesState,
+	useEdgesState,
+	addEdge,
+	MarkerType,
+	ConnectionMode,
+	Edge
 } from 'reactflow';
+
 // 👇 you need to import the reactflow styles
 import 'reactflow/dist/style.css';
 import "./customNodes/NodeStyle.css";
 import "./customEdges/EdgeStyle.css";
-import PlaceNode from './customNodes/TestNode';
+import PlaceNode from './customNodes/PlaceNode';
 import TransitionNode from './customNodes/TransitionNode';
 import ArcEdge from './customEdges/ArcEdge';
+import {getNodeText} from "@storybook/testing-library";
+import {match} from "assert";
 
 
 
@@ -26,14 +30,23 @@ const initialNodes = [
   { id: '4', type: 'Place', position: { x: 500, y: 500 }, data: { label: '1' } },
   { id: '5', type: 'Transition', position: { x: 400, y: 300 }, data: { label: '1' } },
 ];
-const nodeTypes = { Place: PlaceNode, Transition: TransitionNode };
-const edgeTypesC = {
+
+const nodeTypes = {
+  Place: PlaceNode,
+  Transition: TransitionNode
+};
+
+const edgeTypes = {
   Arc: ArcEdge,
 };
 
 
-const initialEdges = [{ id: 'e1-3', type: 'Arc', source: '1', target: '3', sourceHandle: 'c',
-targetHandle: 'a',  markerEnd: { type: MarkerType.ArrowClosed },}];
+const initialEdges = [
+    {
+        id: 'e1-3', type: 'Arc', source: '1', target: '3', sourceHandle: 'c',
+        targetHandle: 'a',  markerEnd: { type: MarkerType.ArrowClosed }
+    }
+    ];
 
 export default Flow;
 
@@ -42,8 +55,57 @@ function Flow() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback((params:any) => setEdges((eds) =>  
-              addEdge({ ...params, type: 'Arc', markerEnd: { type: MarkerType.ArrowClosed } }, eds)), [setEdges]);
+	function get_node(nodeId: string) {
+		return nodes.find((node) => {
+			if (node.id == nodeId) {
+				return node;
+			}
+		})
+	}
+
+  const onConnect = useCallback(
+		(params:any) =>
+			setEdges((eds : (Edge<any>) []) =>
+				{
+					let sourceNode = get_node(params.source);
+					let targetNode = get_node(params.target);
+
+					let is_invalid = edges.every(edge =>
+					{
+						// checking if another edge already exists
+						if (edge.source == params.source && edge.target == params.target) return true;
+
+						return false;
+					})
+
+					// returning without the added note if invalid found
+					if (is_invalid) {
+						return eds;
+					}
+
+					// Checking both nodes exists
+					if (sourceNode == undefined || targetNode == undefined) return eds;
+
+					// Checking if it is valid connection for a place node
+					if (sourceNode.type == 'Place') {
+						if (targetNode.type == 'Place') {
+							return eds;
+						}
+					}
+
+					// Checking if it is valid connection for a transition node
+					if (sourceNode.type == 'Transition') {
+						if (targetNode.type == 'Transition') {
+							return eds;
+						}
+					}
+
+					// Add edge to the list of edges
+					return addEdge({ ...params, type: 'Arc', markerEnd: { type: MarkerType.ArrowClosed } }, eds);
+				}
+			),
+		[setEdges]
+    );
   
   const fitViewOptions = { padding: 0.2 };
 
@@ -55,7 +117,7 @@ function Flow() {
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
       nodeTypes={nodeTypes}
-      edgeTypes={edgeTypesC}
+      edgeTypes={edgeTypes}
       connectionMode = {ConnectionMode.Loose} 
       fitView
       fitViewOptions={fitViewOptions}
---
+++ packages/synthchron-tauri/src/components/customNodes/PlaceNode.tsx
@@ -0,0 +1,40 @@
+import { useCallback } from 'react';
+import {Handle, MarkerType, Position, useReactFlow} from 'reactflow';
+
+const handleStyle = { left: 10 };
+
+//@ts-ignore
+function PlaceNode({ data, isConnectable } ) {
+
+    const reactFlowInstance = useReactFlow();
+
+    function connect(params: any) {
+			// if you want to do something on connect....
+    }
+
+  return (
+    <div className="MeaningLess">
+      <div>
+        <label htmlFor="text"></label>
+      </div>
+      <div className="tokenContainer">
+        <div className="token"></div>
+        <div className="token"></div>
+        <div className="token"></div>
+        <div className="token"></div>
+        <div className="token"></div>
+        <div className="token"></div>
+        <div className="token"></div>
+        <div className="token"></div>
+        <div className="token"></div>
+      </div>
+      <Handle onConnect={(params: any) => connect(params)} type="source" position={Position.Top} id="a" isConnectable={isConnectable} />
+      <Handle onConnect={(params: any) => connect(params)} type="source" position={Position.Bottom} id="b" isConnectable={isConnectable} />
+      <Handle onConnect={(params: any) => connect(params)} type="source" position={Position.Right} id="c" isConnectable={isConnectable} />
+      <Handle onConnect={(params: any) => connect(params)} type="source" position={Position.Left} id="d" isConnectable={isConnectable} />
+    </div>
+  );
+}
+
+
+export default PlaceNode;
