ab7d91bedd374d271f6d7ff0d36dc3720c0cd8ee
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-23 21:20:41 +0100

    export simulation with specific engine

--- packages/synthchron-simulator/src/index.ts
+++ packages/synthchron-simulator/src/index.ts
@@ -1,6 +1,6 @@
 export const main = (): string => 'Hello World';
 
 // Top level simulate command. Can take a configuration dictionary
-export { simulate } from './simulation';
+export { simulate, simulateWithEngine } from './simulation';
 
 export * from './types/processModel';
