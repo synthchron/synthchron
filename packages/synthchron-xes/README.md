67e85a73832eacf6242cfded4ae435a1b70e7f68
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-23 11:27:10 +0100

    add boilerplate for xes synthesizer

---
+++ packages/synthchron-xes/README.md
@@ -0,0 +1,3 @@
+# synthchron-xes
+
+A helper library for generating XES files from abstract representations.
---
+++ packages/synthchron-xes/src/types.ts
@@ -0,0 +1,18 @@
+export type XESLog = {
+    traces: XESTrace[]
+}
+
+export type XESTrace = {
+    events: XESEvent[]
+}
+
+export type XESEvent = {
+    attributes: XESAttribute[]
+}
+
+export type XESAttribute = {
+    key: string
+    value: string
+}
+
+// TODO: Add full XES specs
