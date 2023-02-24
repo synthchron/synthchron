eaabbce2cd75979c8a7997c5131504b4f00a0136
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-24 15:10:35 +0100

    update to webrtc

--- yjs-rf/src/collaborative-example/ydoc.ts
+++ yjs-rf/src/collaborative-example/ydoc.ts
@@ -3,8 +3,12 @@ import { Doc } from 'yjs';
 // between multiple clients. Other providers are available.
 // You can find a list here: https://docs.yjs.dev/ecosystem/connection-provider
 import { WebsocketProvider } from 'y-websocket';
+import { WebrtcProvider } from 'y-webrtc';
+
 
 const ydoc = new Doc();
-const wsProvider = new WebsocketProvider('ws://94.16.117.1/', 'my-roomname', ydoc)
+const wsProvider = new WebrtcProvider('my-roomname', ydoc, {
+    signaling: ['wss://94.16.117.1/']
+})
 
 export default ydoc;
