cd9e86f1aac629a41513d454100fe22f49b8982a
Lennart Mischnaewski <lenmisch@gmail.com>
2023-02-24 23:00:26 +0100

    update ydoc

--- yjs-rf/src/collaborative-example/ydoc.ts
+++ yjs-rf/src/collaborative-example/ydoc.ts
@@ -2,13 +2,12 @@ import { Doc } from 'yjs';
 // For this example we use the WebrtcProvider to synchronize the document
 // between multiple clients. Other providers are available.
 // You can find a list here: https://docs.yjs.dev/ecosystem/connection-provider
-import { WebsocketProvider } from 'y-websocket';
 import { WebrtcProvider } from 'y-webrtc';
 
-
+localStorage.log = 'true'
 const ydoc = new Doc();
-const wsProvider = new WebrtcProvider('my-roomname', ydoc, {
-    signaling: ['wss://94.16.117.1/']
+const wsProvider = new WebrtcProvider('my-roomname-142', ydoc, {
+    signaling: ['ws://94.16.117.1:4444/']
 })
 
 export default ydoc;
