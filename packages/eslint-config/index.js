b24bf0bdef80960cc4cb644b307b6a12e1b9a064
Lennart Mischnaewski <lenmisch@gmail.com>
2023-03-06 16:40:14 +0100

    add eslint and prettierrc

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    //'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    'max-len': ['warn', { code: 120 }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn', // or "error"
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
