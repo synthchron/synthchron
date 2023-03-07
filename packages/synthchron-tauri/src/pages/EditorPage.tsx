d203eff4f8bd96b4a547247f60bf34c820248bcb
Lennart Mischnaewski <lenmisch@gmail.com>
2023-03-07 12:44:11 +0100

    refactor flow components

--- packages/synthchron-tauri/src/pages/EditorPage.tsx
+++ packages/synthchron-tauri/src/pages/EditorPage.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material'
 import { CustomAppBar } from '../components/CustomAppBar'
-import Flow from '../components/Flow'
+import Flow from '../components/react-flow/Flow'
 
 export const EditorPage = () => {
   return (
