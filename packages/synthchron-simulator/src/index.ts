689671716fdaf94794e927d4337420675e3de84a
Lennart Mischnaewski <lenmisch@gmail.com>
2023-03-07 09:55:25 +0100

    remove illegal semicolon

--- packages/synthchron-simulator/src/index.ts
+++ packages/synthchron-simulator/src/index.ts
@@ -3,6 +3,6 @@ export const main = (): string => 'Hello World'
 // Top level simulate command. Can take a configuration dictionary
 export { simulate, simulateWithEngine } from './simulation'
 
-export * from './types/processModel';
+export * from './types/processModel'
 
-export * from './model-examples/petriNetExamples'
+export * from './model-examples/petriNetExamples';
