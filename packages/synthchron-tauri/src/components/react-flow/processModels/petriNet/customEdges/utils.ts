0d61dcaa6ece78162304cb7b54dfc23b251974f0
Lennart Mischnaewski <lenmisch@gmail.com>
2023-03-07 17:15:25 +0100

    add transition node, and update edge connector

--- packages/synthchron-tauri/src/components/react-flow/processModels/petriNet/customEdges/utils.ts
+++ packages/synthchron-tauri/src/components/react-flow/processModels/petriNet/customEdges/utils.ts
@@ -1,62 +1,48 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { Position, internalsSymbol } from 'reactflow'
+import { Position, internalsSymbol, Node } from 'reactflow'
 
-// returns the position (top,right,bottom or right) passed node compared to
+function getClosestHandle(source: Node, target: Node) {
+  const targetCenter = getNodeCenter(target)
+  const sourceCenter = getNodeCenter(source)
 
-//@ts-ignore
-function getParams(nodeA, nodeB) {
-  const centerA = getNodeCenter(nodeA)
-  const centerB = getNodeCenter(nodeB)
+  const targetHandle = target[internalsSymbol]?.handleBounds?.source
+    ?.sort(
+      (h1, h2) =>
+        (h1.x + targetCenter.x - sourceCenter.x) ** 2 +
+        (h1.y + targetCenter.y - sourceCenter.y) ** 2 -
+        (h2.x + targetCenter.x - sourceCenter.x) ** 2 -
+        (h2.y + targetCenter.y - sourceCenter.y) ** 2
+    )
+    .at(0)
 
-  const horizontalDiff = Math.abs(centerA.x - centerB.x)
-  const verticalDiff = Math.abs(centerA.y - centerB.y)
-
-  let position
-
-  // when the horizontal difference between the nodes is bigger, we use Position.Left or Position.Right for the handle
-  if (horizontalDiff > verticalDiff) {
-    position = centerA.x > centerB.x ? Position.Left : Position.Right
-  } else {
-    // here the vertical difference between the nodes is bigger, so we use Position.Top
-    // or Position.Bottom for the handle
-    position = centerA.y > centerB.y ? Position.Top : Position.Bottom
+  if (!targetHandle) {
+    return [0, 0, Position.Right]
   }
 
-  const [x, y] = getHandleCoordsByPosition(nodeA, position)
-  return [x, y, position]
-}
-
-//@ts-ignore
-function getHandleCoordsByPosition(node, handlePosition) {
-  // all handles are from type source, that's why we use handleBounds.source here
-  const handle = node[internalsSymbol].handleBounds.source.find(
-    //@ts-ignore
-    (h) => h.position === handlePosition
-  )
-
-  let offsetX = handle.width / 2
-  let offsetY = handle.height / 2
+  let offsetX = targetHandle.width / 2
+  let offsetY = targetHandle.height / 2
 
   // this is a tiny detail to make the markerEnd of an edge visible. The handle position that gets calculated has the origin top-left, so depending which side we are using, we add a little offset when the handlePosition is Position.Right for example, we need to add an offset as big as the handle itself in order to get the correct position
-  switch (handlePosition) {
+  switch (targetHandle.position) {
     case Position.Left:
       offsetX = 0
       break
     case Position.Right:
-      offsetX = handle.width
+      offsetX = targetHandle.width
       break
     case Position.Top:
       offsetY = 0
       break
     case Position.Bottom:
-      offsetY = handle.height
+      offsetY = targetHandle.height
       break
   }
 
-  const x = node.positionAbsolute.x + handle.x + offsetX
-  const y = node.positionAbsolute.y + handle.y + offsetY
-
-  return [x, y]
+  return [
+    (target.positionAbsolute?.x ?? 0) + targetHandle.x + offsetX,
+    (target.positionAbsolute?.y ?? 0) + targetHandle.y + offsetY,
+    targetHandle.position,
+  ]
 }
 
 //@ts-ignore
@@ -67,18 +53,16 @@ function getNodeCenter(node) {
   }
 }
 
-// returns the parameters (sx, sy, tx, ty, sourcePos, targetPos) you need to create an edge
-//@ts-ignore
-export function getEdgeParams(source, target) {
-  const [sx, sy, sourcePos] = getParams(source, target)
-  const [tx, ty, targetPos] = getParams(target, source)
+export const getEdgeParams = (source: Node, target: Node) => {
+  const [sx, sy, sourcePos] = getClosestHandle(target, source)
+  const [tx, ty, targetPos] = getClosestHandle(source, target)
 
   return {
-    sx,
-    sy,
-    tx,
-    ty,
-    sourcePos,
-    targetPos,
+    sx: sx as number,
+    sy: sy as number,
+    tx: tx as number,
+    ty: ty as number,
+    sourcePos: sourcePos as Position,
+    targetPos: targetPos as Position,
   }
 }
