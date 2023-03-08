cd282bfd2d85772477ee6d6d67baaddd56eb6027
daresoul <nicolas.dyhrman@gmail.com>
2023-03-08 12:24:07 +0100

    Added lint command

--- packages/synthchron-tauri/src/Types/project.ts
+++ packages/synthchron-tauri/src/Types/project.ts
@@ -1,15 +1,15 @@
-import {ProcessModel} from "@synthchron/simulator/dist/src";
+import { ProcessModel } from '@synthchron/simulator/dist/src'
 
 export type Project = {
-	// Name of the project
-	projectName: string,
+  // Name of the project
+  projectName: string
 
-	// The process model, also containing the model type
-	projectModel: ProcessModel,
+  // The process model, also containing the model type
+  projectModel: ProcessModel
 
-	// When the project was created.
-	created: Date,
+  // When the project was created.
+  created: Date
 
-	// When the data has last been saved
-	lastEdited: Date
+  // When the data has last been saved
+  lastEdited: Date
 }
