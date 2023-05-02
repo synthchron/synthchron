f67bd5ad0c540fcdeee34f3d7d7e864c062cffd9
Lennart Mischnaewski <lenmisch@gmail.com>
2023-05-02 16:40:03 +0200

    refactor editor slice

--- packages/synthchron-tauri/src/components/editor/editorStore/editorSlice.ts
+++ packages/synthchron-tauri/src/components/editor/editorStore/editorSlice.ts
@@ -7,6 +7,7 @@ import { EditorState } from './flowStore'
 
 export type EditorSlice = {
   saveFlow: (id: string) => void
+  getProcessModel: () => ProcessModel
 }
 
 export const createEditorSlice: StateCreator<
@@ -16,13 +17,15 @@ export const createEditorSlice: StateCreator<
   EditorSlice
 > = (set, get) => ({
   saveFlow: (id: string) => {
-    const processModel: ProcessModel = get().processModelFlowConfig.serialize(
+    usePersistentStore.getState().updateProject(id, {
+      projectModel: get().getProcessModel(),
+    })
+    set({ unsavedChanges: false })
+  },
+  getProcessModel: () =>
+    get().processModelFlowConfig.serialize(
       get().nodes,
       get().edges,
       get().meta
-    )
-    usePersistentStore.getState().updateProject(id, {
-      projectModel: processModel,
-    })
-  },
+    ),
 })
