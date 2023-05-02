1d04843fcba3a1911810f111eac16f0560840bf1
Lennart Mischnaewski <lenmisch@gmail.com>
2023-05-02 16:38:34 +0200

    update to new react router

--- packages/synthchron-tauri/src/main.tsx
+++ packages/synthchron-tauri/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Routing } from './Routing'
 import './style.css'
@@ -13,8 +13,6 @@ import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routing />
-    </BrowserRouter>
+    <Routing />
   </React.StrictMode>
 )
