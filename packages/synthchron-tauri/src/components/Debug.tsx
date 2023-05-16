1642819fd2ece619870f2355ca2992cab06bae70
Volume999 <Volume99917@gmail.com>
2023-05-16 16:20:27 +0200

    change debug page

--- packages/synthchron-tauri/src/components/Debug.tsx
+++ packages/synthchron-tauri/src/components/Debug.tsx
@@ -1,8 +1,3 @@
-import { Button } from '@mui/material'
-import { SplitPane } from 'react-collapse-pane'
-import { Offline, Online } from 'react-detect-offline'
-
-import { CustomAppBar } from './CustomAppBar'
 import { usePersistentStore } from './common/persistentStore'
 
 export const Debug = () => {
@@ -11,12 +6,10 @@ export const Debug = () => {
 
   return (
     <>
-      <SplitPane split='vertical' collapse={true}>
-        <div>This is the first div</div>
-        <div>This is the second div</div>
-        <div>This is the third div</div>
-        This is the fourth but not a div!
-      </SplitPane>
+      <div>This is the first div</div>
+      <div>This is the second div</div>
+      <div>This is the third div</div>
+      This is the fourth but not a div!
     </>
   )
 }
