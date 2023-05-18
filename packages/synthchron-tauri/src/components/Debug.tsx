d58323e0779e04463ddd69a88fdf0f68aa626cd9
daresoul <nicolas.dyhrman@gmail.com>
2023-05-18 20:56:47 +0200

    works, just tests left

--- packages/synthchron-tauri/src/components/Debug.tsx
+++ packages/synthchron-tauri/src/components/Debug.tsx
@@ -1,5 +1,18 @@
+import { faker } from '@faker-js/faker'
 import { Button } from '@mui/material'
 import { Offline, Online } from 'react-detect-offline'
+
+import {
+  PostProcessingConfiguration,
+  PostProcessingStepType,
+  postprocess,
+} from '@synthchron/postprocessor'
+import {
+  Configuration,
+  StandardConfigurationTerminationType,
+  TerminationType,
+  Trace,
+} from '@synthchron/simulator'
 
 import { CustomAppBar } from './CustomAppBar'
 import { usePersistentStore } from './common/persistentStore'
@@ -7,6 +20,40 @@ import { usePersistentStore } from './common/persistentStore'
 export const Debug = () => {
   const projects = usePersistentStore((state) => state.projects)
   const removeProject = usePersistentStore((state) => state.removeProject)
+
+  const test = () => {
+    const traces: Trace[] = [
+      {
+        events: [
+          { name: 'a', meta: {} },
+          { name: 'b', meta: {} },
+          { name: 'c', meta: {} },
+          { name: 'd', meta: {} },
+          { name: 'e', meta: {} },
+          { name: 'f', meta: {} },
+        ],
+      },
+    ]
+
+    const steps: PostProcessingConfiguration = {
+      stepProbability: 0.5,
+      postProcessingSteps: [
+        { type: PostProcessingStepType.DeletionStep, weight: 1 },
+      ],
+    }
+
+    const config: Configuration = {
+      endOnAcceptingStateProbability: 1,
+      randomSeed: 'abc',
+      terminationType: {
+        type: TerminationType.Standard,
+      } as StandardConfigurationTerminationType,
+    }
+
+    const answer = postprocess(traces, steps, config)
+
+    console.log('Answer: ', answer)
+  }
 
   return (
     <>
@@ -24,6 +71,7 @@ export const Debug = () => {
       >
         Remove all projects
       </Button>
+      <Button onClick={test}>Test</Button>
     </>
   )
 }
