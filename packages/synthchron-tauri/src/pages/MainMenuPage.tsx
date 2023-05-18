79fed338658d936e23d2ee0c0a48edcd7e453d26
Lennart Mischnaewski <lenmisch@gmail.com>
2023-05-18 21:07:13 +0200

    remove unused imports#

--- packages/synthchron-tauri/src/pages/MainMenuPage.tsx
+++ packages/synthchron-tauri/src/pages/MainMenuPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import AddIcon from '@mui/icons-material/Add'
-import { Box, Grid, IconButton, Paper, Toolbar } from '@mui/material'
+import { Box, Grid, IconButton, Paper } from '@mui/material'
 
 import { BottomAppBar } from '../components/BottomAppBar'
 import { CustomAppBar } from '../components/CustomAppBar'
