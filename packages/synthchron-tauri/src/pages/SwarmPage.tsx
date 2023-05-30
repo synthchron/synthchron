33ad9dfc36bea1fdf32588df6047079dcfbb43fa
Lennart Mischnaewski <lenmisch@gmail.com>
2023-05-30 12:44:14 +0200

    add abort to simulation

--- packages/synthchron-tauri/src/pages/SwarmPage.tsx
+++ packages/synthchron-tauri/src/pages/SwarmPage.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { Box, Button, LinearProgress, Stack, Typography } from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress'
 import JSZip from 'jszip'
 
 import {
@@ -34,6 +35,14 @@ export const SwarmPage = () => {
   )
   const [inSimulation, setInSimulation] = useState(false)
 
+  const abortSimulation = useRef(false)
+  useEffect(() => {
+    abortSimulation.current = false
+    return () => {
+      abortSimulation.current = true
+    }
+  }, [])
+
   const simulate = async () => {
     const resultFiles: [string, string, string][] = []
     for (const [projectIndexIndex, projectIndex] of checkedProjects.entries()) {
@@ -55,6 +64,15 @@ export const SwarmPage = () => {
           petriNetEngine
         )
         for await (const { progress, simulationLog } of simulator) {
+          if (abortSimulation.current) {
+            setCurrentModel(undefined)
+            setCurrentConfig(undefined)
+            setProgress(0)
+            setModelProgress(0)
+            setConfigProgress(0)
+            setInSimulation(false)
+            return []
+          }
           setProgress(
             (100 *
               (projectIndexIndex +
@@ -193,6 +211,18 @@ export const SwarmPage = () => {
                 },
               }}
             />
+            <Button
+              variant='contained'
+              fullWidth
+              disabled={abortSimulation.current} // make it error color
+              onClick={() => {
+                abortSimulation.current = true
+                setInSimulation(false)
+              }}
+              color='error'
+            >
+              {abortSimulation.current ? 'Aborting...' : 'Abort Simulation'}
+            </Button>
           </Box>
         ) : (
           <Button
@@ -209,6 +239,11 @@ export const SwarmPage = () => {
               setInSimulation(true)
               setProgress(0)
               simulate().then(async (result: [string, string, string][]) => {
+                if (abortSimulation.current || result.length === 0) {
+                  setInSimulation(false)
+                  abortSimulation.current = false
+                  return
+                }
                 const zip = new JSZip()
                 for (const [projectName, configurationName, xes] of result) {
                   zip.file(`${projectName} - ${configurationName}.xes`, xes, {
