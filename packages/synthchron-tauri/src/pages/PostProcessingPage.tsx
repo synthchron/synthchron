4566b464bf549b718cb8f841ae2d7909357bec9b
Mat Hentia <mathiasarbejdsmail@gmail.com>
2023-06-06 16:28:27 +0200

    Multiple files can be uploaded, however postprocessing only works for one

--- packages/synthchron-tauri/src/pages/PostProcessingPage.tsx
+++ packages/synthchron-tauri/src/pages/PostProcessingPage.tsx
@@ -53,163 +53,202 @@ type UploadedFile = {
 
 export const PostProcessingPage = () => {
   const [postprocessing, setPostprocessing] = useState(defaultPostprocessing)
-  const [uploadedTrace, setUploadedFile] = useState<UploadedFile | null>(null)
+  const [uploadedFiles, setUploadedFiles] = useState<UploadedFile[] | null>(
+    null
+  )
   const [traceText, setTraceText] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
-  const uploadFile = useCallback((acceptedFiles: File[]) => {
-    reset()
-    // The check is done here instead of the dropzone component
-    // in order to display a more specific error message
-    if (acceptedFiles.length > 1) {
-      setErrorMessage('Only one file can be uploaded at a time')
-      return
-    }
-    const file = acceptedFiles[0]
-    if (!file.name.endsWith('.xes')) {
-      setErrorMessage('File must be a .xes file')
-      return
-    }
-    const reader = new FileReader()
-    reader.onload = function () {
-      setUploadedFile({ name: file.name, content: reader.result as string })
-    }
-    reader.readAsText(file)
-  }, [])
+  const uploadFile = useCallback(
+    (acceptedFiles: File[]) => {
+      // The check is done here instead of the dropzone component
+      // in order to display a more specific error message
+      acceptedFiles.forEach((file) => {
+        if (!file.name.endsWith('.xes')) {
+          setErrorMessage('File must be a .xes file')
+          throw file.name + ' was not a .xes file'
+        }
+      })
+
+      acceptedFiles.forEach((file) => {
+        if (
+          !uploadedFiles?.some((loadedFile) => loadedFile.name === file.name)
+        ) {
+          const reader = new FileReader()
+          reader.onload = function () {
+            setUploadedFiles((prevUploadedFiles) => {
+              const newUploadedFile = {
+                name: file.name,
+                content: reader.result as string,
+              }
+              return prevUploadedFiles
+                ? [...prevUploadedFiles, newUploadedFile]
+                : [newUploadedFile]
+            })
+          }
+          reader.readAsText(file)
+        }
+      })
+      console.log(uploadedFiles) //REMOVE
+    },
+    [uploadedFiles]
+  )
 
   const reset = () => {
-    setUploadedFile(null)
+    console.log(uploadedFiles) //REMOVE
+    setUploadedFiles(null)
     setTraceText('')
     setErrorMessage('')
   }
   return (
     <>
       <CustomAppBar />
-      <Grid container style={{ height: 'calc(100vh - 70px)' }}>
-        <Grid item xs={12} md={6}>
-          <Paper style={{ minHeight: '100%' }}>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                height: '45%',
-                overflow: 'auto',
-                marginBottom: '10px',
-              }}
-            >
-              <Typography variant='h6' align={'center'}>
-                Paste your log here
-              </Typography>
-              <Tooltip
-                title={
-                  uploadedTrace !== null
-                    ? 'Disabled while there is an uploaded file'
-                    : ''
-                }
-                arrow
-                placement='left'
-              >
-                <TextField
-                  multiline
-                  fullWidth
-                  maxRows={10}
+
+      <Box
+        sx={{
+          width: '100%',
+          maxWidth: ['95%', '95%', '95%', '95%'],
+          margin: '2em auto',
+        }}
+      >
+        <Grid container style={{ height: 'calc(100vh - 70px)' }} spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Paper sx={{ padding: '1em' }}>
+              <Box style={{ minHeight: '100%' }}>
+                <Box
                   sx={{
-                    flexGrow: 1,
-                    alignItems: 'flex-start',
-                    maxHeight: '50vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '45%',
+                    overflow: 'auto',
+                    marginBottom: '10px',
                   }}
-                  onChange={(event) => {
-                    setTraceText(event.target.value)
-                  }}
-                  disabled={uploadedTrace !== null}
-                />
-              </Tooltip>
-            </Box>
-            <Box>
-              <Typography variant='h6' align='center'>
-                Or upload file
-              </Typography>
-              <Dropzone onDrop={uploadFile} disabled={traceText !== ''}>
-                {({ getRootProps, getInputProps, isDragActive }) => (
-                  <div {...getRootProps()} style={{ backgroundColor: 'grey' }}>
-                    <input {...getInputProps()} />
-                    <Tooltip
-                      title={
-                        traceText === ''
-                          ? 'Drop / Select file'
-                          : 'Disabled while there is input in the text field above'
-                      }
-                      arrow
-                      placement='left'
-                    >
-                      <Paper
-                        style={{
-                          minHeight: 250,
-                          height: '100%',
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          backgroundColor:
-                            traceText !== ''
-                              ? 'gray'
-                              : isDragActive
-                              ? 'lightgray'
-                              : 'whitesmoke',
-                          cursor: 'pointer',
-                        }}
-                      >
-                        <IconButton>
-                          <AddIcon fontSize='large' />
-                        </IconButton>
-                      </Paper>
-                    </Tooltip>
-                  </div>
-                )}
-              </Dropzone>
-              <Button variant='contained' color='primary' onClick={reset}>
-                Reset
-              </Button>
-              {uploadedTrace !== null && (
-                <Box sx={{ backgroundColor: 'lightgreen', padding: '5px' }}>
-                  <Typography variant='body1'>
-                    Successfully uploaded file: {uploadedTrace.name}
+                >
+                  <Typography variant='h6' align={'center'}>
+                    Paste your log here
                   </Typography>
+                  <Tooltip
+                    title={
+                      uploadedFiles !== null
+                        ? 'Disabled while there is an uploaded file'
+                        : ''
+                    }
+                    arrow
+                    placement='left'
+                  >
+                    <TextField
+                      multiline
+                      fullWidth
+                      maxRows={10}
+                      value={traceText}
+                      label='Paste your log here'
+                      sx={{
+                        flexGrow: 1,
+                        alignItems: 'flex-start',
+                        maxHeight: '50vh',
+                      }}
+                      onChange={(event) => {
+                        setTraceText(event.target.value)
+                      }}
+                      disabled={uploadedFiles !== null}
+                    />
+                  </Tooltip>
                 </Box>
-              )}
-              {errorMessage != '' && (
-                <Typography variant='body1'>
-                  <Box sx={{ backgroundColor: 'red', padding: '5px' }}>
-                    {errorMessage}
+                <Box>
+                  <Typography variant='h6' align='center'>
+                    Or upload file
+                  </Typography>
+                  <Dropzone onDrop={uploadFile} disabled={traceText !== ''}>
+                    {({ getRootProps, getInputProps, isDragActive }) => (
+                      <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <Tooltip
+                          title={
+                            traceText === ''
+                              ? 'Drop / Select file'
+                              : 'Disabled while there is input in the text field above'
+                          }
+                          arrow
+                          placement='left'
+                        >
+                          <Paper
+                            style={{
+                              minHeight: 250,
+                              height: '100%',
+                              display: 'flex',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              backgroundColor:
+                                traceText !== ''
+                                  ? 'gray'
+                                  : isDragActive
+                                  ? 'lightgray'
+                                  : 'whitesmoke',
+                              cursor: 'pointer',
+                            }}
+                          >
+                            <IconButton>
+                              <AddIcon fontSize='large' />
+                            </IconButton>
+                          </Paper>
+                        </Tooltip>
+                      </div>
+                    )}
+                  </Dropzone>
+                  <Box textAlign='center' sx={{ marginTop: '1em' }}>
+                    <Button variant='contained' color='primary' onClick={reset}>
+                      Reset
+                    </Button>
                   </Box>
-                </Typography>
-              )}
+                  {uploadedFiles !== null && (
+                    <Box sx={{ backgroundColor: 'lightgreen', padding: '5px' }}>
+                      <Typography variant='body1'>
+                        Successfully uploaded file: {uploadedFiles[0].name}{' '}
+                        {/*FIX*/}
+                      </Typography>
+                    </Box>
+                  )}
+                  {errorMessage != '' && (
+                    <Typography variant='body1'>
+                      <Box sx={{ backgroundColor: 'red', padding: '5px' }}>
+                        {errorMessage}
+                      </Box>
+                    </Typography>
+                  )}
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box>
+              <Box sx={{ minHeight: '100%', marginLeft: 'auto' }}>
+                <PostprocessingPanel
+                  postprocessing={postprocessing}
+                  setPostprocessing={setPostprocessing}
+                />
+                <Button
+                  sx={{ marginTop: '1em' }}
+                  variant='contained'
+                  color='primary'
+                  fullWidth
+                  onClick={() => {
+                    const trace =
+                      uploadedFiles !== null
+                        ? uploadedFiles[0].content //FIX
+                        : traceText
+                    exportStringAsFile(
+                      postProcessXESFile(trace, postprocessing),
+                      'postProcessedEventLog.xes'
+                    )
+                  }}
+                  disabled={traceText === '' && uploadedFiles === null}
+                >
+                  Post Process and Generate XES
+                </Button>
+              </Box>
             </Box>
-          </Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ minHeight: '100%', marginLeft: 'auto' }}>
-            <PostprocessingPanel
-              postprocessing={postprocessing}
-              setPostprocessing={setPostprocessing}
-            />
-            <Button
-              variant='contained'
-              color='primary'
-              fullWidth
-              onClick={() => {
-                const trace =
-                  uploadedTrace !== null ? uploadedTrace.content : traceText
-                exportStringAsFile(
-                  postProcessXESFile(trace, postprocessing),
-                  'postProcessedEventLog.xes'
-                )
-              }}
-              disabled={traceText === '' && uploadedTrace === null}
-            >
-              Post Process and Generate XES
-            </Button>
-          </Paper>
-        </Grid>
-      </Grid>
+      </Box>
     </>
   )
 }
