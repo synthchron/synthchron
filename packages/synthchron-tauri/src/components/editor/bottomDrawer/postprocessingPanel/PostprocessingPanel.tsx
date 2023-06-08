8a6cf99479ad6f84da8b46816419711d2ef50441
Lennart Mischnaewski <lenmisch@gmail.com>
2023-06-08 14:18:28 +0200

    remove unused imports

--- packages/synthchron-tauri/src/components/editor/bottomDrawer/postprocessingPanel/PostprocessingPanel.tsx
+++ packages/synthchron-tauri/src/components/editor/bottomDrawer/postprocessingPanel/PostprocessingPanel.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react'
 
-import { CheckBox } from '@mui/icons-material'
 import HelpIcon from '@mui/icons-material/Help'
-// Temporary
 import {
   Checkbox,
   Collapse,
